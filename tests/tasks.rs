c16f2e59a5df5f8ccfe3087807fca635d049059c
Serge Toropov <sombr@yandex-team.ru>
2016-09-08 23:10:56 +0100

    don`t throw in waiting

--- tests/tasks.rs
+++ tests/tasks.rs
@@ -1,12 +1,15 @@
 extern crate tasks;
 use tasks::tasks::Tasks;
 
+use std::thread::JoinHandle;
+use std::thread::spawn;
+use std::sync::Arc;
+
 #[test]
-#[should_panic]
 fn test_fetch_task_should_not_be_called_on_a_waiting_state() {
     let tasks = Tasks::new();
-    let _ = tasks.fetch_task();
-    assert!(false)
+    let recheck_queue = tasks.fetch_task();
+    assert_eq!(recheck_queue, false)
 }
 
 #[test]
@@ -52,4 +55,29 @@ fn test_fetch_task_returns_recheck_queue_false_for_an_in_flight_state_without_ta
 
     let recheck_queue = tasks.fetch_task();
     assert_eq!(recheck_queue, false)
+}
+
+#[test]
+fn test_multi_thread() {
+    let tasks: Arc<Tasks> = Arc::new(Tasks::new());
+
+    let mut threads: Vec<JoinHandle<()>> = vec!();
+    for i in 0 .. 10 {
+        let tasks_ref = tasks.clone();
+        threads.push(spawn(move || {
+            for j in i .. 10 {
+                if j % 2 == 0 {
+                    let _ = tasks_ref.fetch_task();
+                } else {
+                    let _ = tasks_ref.add_task();
+                }
+            }
+        }));
+    }
+
+    for handle in threads {
+        let _ = handle.join();
+    }
+
+    assert!(true);
 }
