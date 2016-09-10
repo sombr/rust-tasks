601ac3014efebc5c57be12b9d0627f809bddb70f
Serge Toropov <sombr@yandex-team.ru>
2016-09-10 19:40:32 +0100

    test refactor

--- tests/actor_runner.rs
+++ tests/actor_runner.rs
@@ -9,7 +9,7 @@ use std::cell::Cell;
 use std::sync::Arc;
 
 struct Actor {
-    value: Cell<usize>
+    value: Arc<Cell<usize>>
 }
 
 unsafe impl Send for Actor {}
@@ -17,26 +17,36 @@ unsafe impl Sync for Actor {}
 
 impl Actor {
     fn new() -> Actor {
-        Actor { value: Cell::new(0) }
+        Actor { value: Arc::new(Cell::new(0)) }
+    }
+
+    fn get_value_ref(&self) -> Arc<Cell<usize>> {
+        return self.value.clone();
     }
 }
 
 impl Runnable for Actor {
     fn run(&self) {
-        for _ in 0..100 {
+        for _ in 0 .. 10000 {
             self.value.set( self.value.get() + 1 );
-            println!(">> {}", self.value.get());
         }
     }
 }
 
 #[test]
-fn test_fetch_task_should_not_be_called_on_a_waiting_state() {
+fn actor_runner_schedule_with_a_primitive_actor_and_a_threadpool_executes_correctly() {
     let pool = Arc::new(ThreadPool::new(2));
     let actor = Actor::new();
+
+    let val = actor.get_value_ref();
 
     let runner: ActorRunner<Actor> = ActorRunner::new(actor, pool);
 
     runner.schedule();
     runner.schedule();
+    runner.schedule();
+
+    runner.complete();
+
+    assert_eq!( val.get(), 10000 );
 }
